e0971693f4856fa3a7451f46fca6d1927af41dbc
Frank Nkurunziza <franknkurunzia@gmail.com>
2021-06-16 19:09:03 -0400

    initial commit for user and bank account

--- bank_account.py
+++ bank_account.py
@@ -1,34 +1,34 @@
-class BankAccount:
-    def __init__(self,int_rate,balance):
-        self.rate=int_rate
-        self.account_balance=balance
+# class BankAccount:
+#     def __init__(self,int_rate,balance):
+#         self.rate=int_rate
+#         self.account_balance=balance
         
-    def deposit(self,amount):
-        self.account_balance +=amount
-        return self
+#     def deposit(self,amount):
+#         self.account_balance +=amount
+#         return self
 
-    def withdraw(self,amount):
-        if self.account_balance>=amount:
-            self.account_balance -=amount
-        else:
-            print("insufficient funds you will be charged $5")
-            self.account_balance -=5
-        return self
+#     def withdraw(self,amount):
+#         if self.account_balance>=amount:
+#             self.account_balance -=amount
+#         else:
+#             print("insufficient funds you will be charged $5")
+#             self.account_balance -=5
+#         return self
 
-    def display_account_info(self):
-        print(f'you have {self.account_balance} in your acoount')
-        # return self
+#     def display_account_info(self):
+#         print(f'you have {self.account_balance} in your acoount')
+#         # return self
 
-    def yield_interest(self):
-        if self.account_balance>0:
-            yield_amount=self.account_balance *self.rate
-            self.account_balance+=yield_amount
-        return self
+#     def yield_interest(self):
+#         if self.account_balance>0:
+#             yield_amount=self.account_balance *self.rate
+#             self.account_balance+=yield_amount
+#         return self
 
-Acc1=BankAccount(0.01,100)
-Acc1.deposit(100).deposit(50).deposit(30).withdraw(20).yield_interest().display_account_info()
+# Acc1=BankAccount(0.01,100)
+# Acc1.deposit(100).deposit(50).deposit(30).withdraw(20).yield_interest().display_account_info()
 
-Acc2=BankAccount(0.01,0)
-Acc2.deposit(500).deposit(500).withdraw(50).withdraw(150).withdraw(50).withdraw(250).yield_interest().yield_interest()
+# Acc2=BankAccount(0.01,0)
+# Acc2.deposit(500).deposit(500).withdraw(50).withdraw(150).withdraw(50).withdraw(250).yield_interest().yield_interest()
 
 
--- main.py
+++ main.py
@@ -3,38 +3,61 @@ class User:
     def __init__(self,nameOfUser, email):
         self.name=nameOfUser
         self.email=email
-        self.account_balance=0
+        self.bankaccounts=[]
 
-    def make_deposit(self,amount):
+    def another_acc(self,rate=0.02,balance=0):# default parameter
+        self.bankaccounts.append(BankAccount(rate,balance))
+        return self
+    def made_deposit(self,amount,acc_number):
+        self.bankaccounts[acc_number].deposit(amount)
+        return self
+
+    def make_withdraw(self,amount,acc_number):
+        self.bankaccounts[acc_number].withdraw(amount)
+        return self
+
+    def display(self,acc_number):
+        self.bankaccounts[acc_number].display_account_info()
+        return self
+
+    def display_all_account(self):
+        for x in range(len(self.bankaccounts)):
+            print(f'ACCOUNT #:{x}')
+            self.bankaccounts[x].display_account_info()
+        return self
+
+class BankAccount:
+    def __init__(self,int_rate,balance):
+        self.rate=int_rate
+        self.account_balance=balance
+        
+    def deposit(self,amount):
         self.account_balance +=amount
         return self
 
-    def withdrow(self,amount):
-        self.account_balance -= amount
+    def withdraw(self,amount):
+        if self.account_balance>=amount:
+            self.account_balance -=amount
+        else:
+            print("insufficient funds you will be charged $5")
+            self.account_balance -=5
         return self
 
-    def transfer_fund(self,amount,other_user):
-        self.account_balance -= amount
-        other_user.account_balance += amount
+    def display_account_info(self):
+        print(f'you have {self.account_balance} in your acoount')
+        # return self
+
+    def yield_interest(self):
+        if self.account_balance>0:
+            yield_amount=self.account_balance *self.rate
+            self.account_balance+=yield_amount
         return self
-    
-    def display_balance(self):
-        print(f'{self.name} have a balance of {self.account_balance}')
 
 user1=User("nkurunziza","nkurunziza@bank.com")
 user2=User("kanye","kanye@bank.com")
 user3=User("doe","doe@bank.com")
 
-user1.make_deposit(100).make_deposit(100).make_deposit(100).withdrow(20).transfer_fund(40,user3)
-
-user2.make_deposit(400).make_deposit(400).withdrow(150).withdrow(150)
-
-user3.make_deposit(500).withdrow(50).withdrow(50).withdrow(50)
-
-
-user1.display_balance()
-user2.display_balance()
-user3.display_balance()
-
+user1.another_acc(0.05,200).made_deposit(200,0)
+user1.another_acc(0.04,300).another_acc(00.1,500).display_all_account()
 
 
